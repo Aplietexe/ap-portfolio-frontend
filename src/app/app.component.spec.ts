74d99435b99af567371532f97ecc867d15bd379d
Pietro Palombini <pietropalo@icloud.com>
2023-02-24 03:14:41 -0300

    Set up project

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -1,31 +1,35 @@
-import { TestBed } from '@angular/core/testing';
-import { AppComponent } from './app.component';
+import { TestBed } from "@angular/core/testing"
 
-describe('AppComponent', () => {
+import { AppComponent } from "./app.component"
+
+describe("AppComponent", () => {
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      declarations: [
-        AppComponent
-      ],
-    }).compileComponents();
-  });
+      declarations: [AppComponent],
+    }).compileComponents()
+  })
 
-  it('should create the app', () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    const app = fixture.componentInstance;
-    expect(app).toBeTruthy();
-  });
+  it("should create the app", () => {
+    const fixture = TestBed.createComponent(AppComponent)
+    const app = fixture.componentInstance
 
-  it(`should have as title 'ap-portfolio-frontend'`, () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    const app = fixture.componentInstance;
-    expect(app.title).toEqual('ap-portfolio-frontend');
-  });
+    expect(app).toBeTruthy()
+  })
 
-  it('should render title', () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    fixture.detectChanges();
-    const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('.content span')?.textContent).toContain('ap-portfolio-frontend app is running!');
-  });
-});
+  it("should have as title 'ap-portfolio-frontend'", () => {
+    const fixture = TestBed.createComponent(AppComponent)
+    const app = fixture.componentInstance
+
+    expect(app.title).toEqual("ap-portfolio-frontend")
+  })
+
+  it("should render title", () => {
+    const fixture = TestBed.createComponent(AppComponent)
+    fixture.detectChanges()
+    const compiled = fixture.nativeElement as HTMLElement
+
+    expect(compiled.querySelector(".content span")?.textContent).toContain(
+      "ap-portfolio-frontend app is running!",
+    )
+  })
+})
